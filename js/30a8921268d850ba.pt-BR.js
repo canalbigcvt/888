22a90f29fb93bae04f51bd06f6fefd6d5e533a95
canalbigcvt <canalbigcvt@gmail.com>
2025-02-23 20:59:55 -0300

    Add files via upload

---
+++ js/30a8921268d850ba.pt-BR.js
@@ -0,0 +1,2 @@
+window['cmsg']['assets'] = window['cmsg']['assets'] || {};
+window['cmsg']['assets']["pt-BR"] = Object.assign(window['cmsg']['assets']["pt-BR"] || {}, {"15706":{"js":["6018f2c7a77c016c.strings.js"],"css":[]},"38701":{"js":["1f16512b643281d7.strings.js"],"css":[]}});
